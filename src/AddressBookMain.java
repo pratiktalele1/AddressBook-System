4cc9c4fc8a34592efe792b9e56305627e919725b
Pratik Talele <pratiktalele22@gmail.com>
2021-07-16 17:56:25 +0530

    commit add contact of person

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,5 +1,26 @@
 
 public class AddressBookMain {
 
-	
+	//getting command line arguments
+	public static void main(String[] args) {
+		Person person = new Person();
+		int i = 0;
+
+		//setting value to variables
+		person.setFirstName(args[i++]);
+		person.setLastName(args[i++]);
+		person.setAddress(args[i++]);
+		person.setCityName(args[i++]);
+		person.setStateName(args[i++]);
+		person.setZipCode(args[i++]);
+		person.setPhoneNumber(args[i++]);
+		person.setEmailAddress(args[i++]);
+
+		//displaying person contact
+		System.out.println("first Name ::" + person.getFirstName() + " Last Name ::" + person.getLastName()
+				+ " address ::" + person.getAddress() + " city Name ::" + person.getCityName() + "\nstate Name ::"
+				+ person.getStateName() + " zip ::" + person.getZipCode() + " phone ::" + person.getPhoneNumber()
+				+ " email ::" + person.getEmailAddress());
+	}
+
 }
--- src/Person.java
+++ src/Person.java
@@ -1,11 +1,64 @@
 
 public class Person {
+	//person contact fields
 	private String firstName;
 	private String lastName;
 	private String address;
 	private String cityName;
 	private String stateName;
-	private long zipCode;
-	private long phoneNumber;
+	private String zipCode;
+	private String phoneNumber;
 	private String emailAddress;
+	
+	//getter and setter for contact 
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	public String getStateName() {
+		return stateName;
+	}
+	public void setStateName(String stateName) {
+		this.stateName = stateName;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	
+	
 }
